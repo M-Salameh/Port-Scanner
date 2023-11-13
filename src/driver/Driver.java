f767693b1afe1f5f53db66c1695570eb4420ee87
mohammad.salama <mohammad.salama@hiast.edu.sy>
2023-11-13 12:26:26 -0500

    Working with user Input

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -28,13 +29,34 @@ public class Driver {
     private static final int SCAN_TIMEOUT = 200;
     private static final int THREAD_COUNT = 128;
 
+    private static String[] takeInput()
+    {
+        String[] Args = new String[3];
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter IPV4 to START scanning from");
+        Args[0] = scanner.nextLine();
+        System.out.println("Enter IPV4 to END scanning with");
+        Args[1] = scanner.nextLine();
+        String a="";
+        System.out.println("Enter Start PORT to START scanning from");
+        a = scanner.next();
+        Args[2] = a+"-";
+        System.out.println("Enter Start PORT to END scanning with");
+        a = scanner.next();
+        Args[2] += a;
+        System.out.println(Args[0]);
+        System.out.println(Args[1]);
+        System.out.println(Args[2]);
+
+        return Args;
+    }
+
     public static void main(String[] args) {
         // get a Target info from Parser
         List<Target> targets = new ArrayList<>();
-        String[] Args = new String[3];
-        Args[0] = "8.8.8.4";
-        Args[1] = "8.8.8.9";
-        Args[2] = "19-80";
+        String[] Args = takeInput();
+        /*
         try {
             targets = Parser.run(Args);
         } catch (InvalidUserInputException e) {
@@ -47,6 +69,6 @@ public class Driver {
         for (Target t : targets)
         {
             SingleIPDriver.start(t);
-        }
+        }*/
     }
 }
